ddd9f9224678e18cbaf3cc1fa554a696df10e434
AJ ONeal <coolaj86@gmail.com>
2021-09-12 03:53:43 -0600

    refactor!: more robust config, auth, and structure

--- app.js
+++ app.js
@@ -1,73 +1,96 @@
 "use strict";
 
+let config = require("./config.js");
+
+if ("development" === process.env.NODE_ENV) {
+  // set special options
+  console.info("NODE_ENV=" + process.env.NODE_ENV);
+}
+
+let pkg = require("./package.json");
+
 let app = require("@root/async-router").Router();
+let Errors = require("./lib/errors.js");
+
 let Keyfetch = require("keyfetch");
 let request = require("@root/request");
 
-if ("DEVELOPMENT" === process.env.ENV) {
-  // set special options
-  console.info("ENV=" + process.env.ENV);
-}
+let Cors = require("./lib/cors.js");
+let cors = Cors({ domains: config.CORS_DOMAINS, methods: config.CORS_METHODS });
 
-function Err({ message, code, status, details }) {
-  let err = new Error(message);
-  err.code = code;
-  err.status = status;
-  err.details = details;
-  return err;
-}
+let Public = require("./lib/public.js")(pkg, config);
 
-app.use("/", function (req, res, next) {
-  // use a debug / pretty print json
-  res.json = function (data) {
-    res.setHeader("Content-Type", "application/json");
-    let json = JSON.stringify(data, null, 2);
-    res.end(json + "\n");
-  };
+// Pretty JSON output
+app.use("/", addReqJsonPretty);
+
+// CORS
+app.use("/", cors);
+app.options("*", endRequest);
+
+//app.use("/api", require("body-parser").json());
+
+// Public / Debug endpoints
+app.get("/public/hello", Public.getHello);
+app.get("/public/versions", Public.getVersions);
+app.get("/public/envs", Public.getPublicEnvs);
+
+// OIDC / Auth Debug endpoints
+app.use("/public/oidc", decodeClaims);
+app.get("/public/oidc/inspect", async function (req, res) {
+  res.json(req.jws);
+});
+app.get("/public/oidc/config", getConfig, async function (req, res) {
+  res.json(req.oidcConfig);
+});
+app.get("/public/oidc/profile", getConfig, getProfile);
+
+// Error / Debug endpoints
+app.get("/public/errors/400", async function (req, res) {
+  throw Errors.create("BAD_REQUEST");
+});
+app.get("/public/errors/500", async function (req, res) {
+  throw Errors.create("some rando error");
+});
+app.get("/public/errors/404", async function (req, res, next) {
   next();
 });
 
-app.get("/", async function (req, res) {
-  res.end(
-    `GET /
-GET /hello
-GET /envs
-GET /oidc/config
-GET /oidc/inspect
-GET /oidc/profile
-GET /errors/400
-GET /errors/404
-GET /errors/500
-`
-  );
-});
+// Magic Sauce endpoints
+app.use("/user", decodeClaims, verifyClaims);
 
-app.get("/hello", async function (req, res) {
-  res.json({
-    message: "Hello, World!",
+// TODO error handler for /api
+app.use("/", function (req, res) {
+  throw Errors.custom({
+    code: "NOT_FOUND",
+    status: 404,
+    message: `'${req.url}' not found`,
   });
 });
+app.use("/", handleErrorJson);
 
-app.get("/envs", async function (req, res) {
-  res.json({
-    env: process.env.ENV || "",
-    port: process.env.PORT || 0,
-  });
-});
+function endRequest(req, res, next) {
+  res.end();
+}
 
-app.use("/oidc", decodeClaims);
+// default API error handler
+async function handleErrorJson(err, req, res, next) {
+  err.id = require("crypto").randomBytes(3).toString("hex");
 
-app.get("/oidc/inspect", async function (req, res) {
-  res.json(req.jws);
-});
+  if (err.status >= 400 && err.status < 500) {
+    res.statusCode = err.status || 500;
+    res.json({ status: err.status, code: err.code, message: err.message });
+    return;
+  }
 
-app.get("/oidc/config", getConfig, async function (req, res) {
-  res.json(req.oidcConfig);
-});
+  console.error("Unexpected Error:");
+  console.error(err);
+  res.statusCode = err.status || 500;
+  res.json({ message: `Internal Server Error: #${err.id}`, id: err.id });
+}
 
-app.get("/oidc/profile", getConfig, async function (req, res) {
+async function getProfile(req, res) {
   if (!req.oidcConfig.userinfo_endpoint) {
-    throw Err({
+    throw Errors.custom({
       message:
         "OpenID Configuration is mising userinfo_endpoint - try inspecting with /oidc/config",
       status: 422,
@@ -84,7 +107,7 @@ app.get("/oidc/profile", getConfig, async function (req, res) {
     .catch(function (err) {
       console.error(`Could not get '${req.oidcConfig.userinfo_endpoint}':`);
       console.error(err);
-      throw Err({
+      throw Errors.custom({
         message:
           "could not fetch OpenID Configuration - try inspecting the token and checking 'iss'",
         status: 422,
@@ -93,44 +116,12 @@ app.get("/oidc/profile", getConfig, async function (req, res) {
     });
 
   res.json(resp.body);
-});
-
-app.get("/errors/400", async function (req, res) {
-  let err = new Error("BAD REQUEST");
-  err.code = "E_BAD_REQUEST";
-  err.status = 400;
-  err.details = "Yeah, it's your fault";
-  throw err;
-});
-
-app.get("/errors/500", async function (req, res) {
-  throw Error("some rando error");
-});
-
-app.get("/errors/404", async function (req, res, next) {
-  next();
-});
-
-// TODO error handler for /api
-app.use("/", async function (err, req, res, next) {
-  err.id = require("crypto").randomBytes(3).toString("hex");
-
-  if (err.status >= 400 && err.status < 500) {
-    res.statusCode = err.status || 500;
-    res.json({ status: err.status, code: err.code, message: err.message });
-    return;
-  }
-
-  console.error("Unexpected Error:");
-  console.error(err);
-  res.statusCode = err.status || 500;
-  res.json({ message: `Internal Server Error: #${err.id}\n`, id: err.id });
-});
+}
 
 async function decodeClaims(req, res, next) {
   let token = (req.headers["authorization"] || "").split(" ")[1];
   if (!token) {
-    throw Err({
+    throw Errors.custom({
       message:
         "missing token header, example: 'Authorization: Bearer xxxx.yyyy.zzzz'",
       code: "E_MISSING_AUTHORIZATION",
@@ -144,16 +135,42 @@ async function decodeClaims(req, res, next) {
   next();
 }
 
+function ensureTrailingSlash(iss) {
+  if (!iss || "string" !== typeof iss) {
+    throw Errors.create("'iss' should be an oidc issuer url string");
+  }
+  if (!iss.endsWith("/")) {
+    iss += "/";
+  }
+  return iss;
+}
+
+async function verifyClaims(req, res, next) {
+  if ("development" === config.NODE_ENV && config.INSECURE_SKIP_VERIFY) {
+    console.warn("[SECURITY] Skipping token verification");
+    next();
+    return;
+  }
+
+  var oidcUrl = ensureTrailingSlash(req.claims.iss);
+  if (config.OIDC_ISSUER !== oidcUrl) {
+    throw Errors.custom({
+      code: "E_UNKNOWN_ISSUER",
+      message: "token is from an unknown or invalid OIDC issuer",
+      status: 401,
+    });
+  }
+
+  req.jws = await Keyfetch.jwt.verify(req.jwt);
+  req.claims = req.jws.claims;
+  next();
+}
+
 async function mustOk(resp) {
   if (resp.statusCode >= 200 && resp.statusCode < 300) {
     return resp;
   }
-  throw Err({
-    message: "remote server gave a non-OK response",
-    code: "E_BAD_GATEWAY",
-    status: 502,
-    details: resp.body,
-  });
+  throw Errors.create("BAD_GATEWAY");
 }
 
 async function getConfig(req, res, next) {
@@ -166,10 +183,7 @@ async function getConfig(req, res, next) {
     return err;
   }
 
-  let oidcUrl = req.claims.iss.toString();
-  if (!oidcUrl.endsWith("/")) {
-    oidcUrl += "/";
-  }
+  let oidcUrl = ensureTrailingSlash(req.claims.iss);
   oidcUrl += ".well-known/openid-configuration";
 
   // See examples:
@@ -181,7 +195,7 @@ async function getConfig(req, res, next) {
     .catch(function (err) {
       console.error(`Could not get '${oidcUrl}':`);
       console.error(err);
-      throw Err({
+      throw Errors.custom({
         message:
           "could not fetch OpenID Configuration - try inspecting the token and checking 'iss'",
         status: 422,
@@ -190,6 +204,16 @@ async function getConfig(req, res, next) {
     });
 
   req.oidcConfig = resp.body;
+  next();
+}
+
+function addReqJsonPretty(req, res, next) {
+  // use a debug / pretty print json
+  res.json = function (data) {
+    res.setHeader("Content-Type", "application/json");
+    let json = JSON.stringify(data, null, 2);
+    res.end(json + "\n");
+  };
   next();
 }
 
@@ -207,4 +231,5 @@ if (require.main === module) {
   });
 }
 
+module.exports = app;
 module.exports = app;
